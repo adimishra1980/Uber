ca76ef91edc04ecf957a525564e50460b58e0603
Aditya Mishra <adityamishra19880@gmail.com>
2024-11-30 15:48:02 +0530

    user authentication done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
+const { authenticateUser } = require("../middlewares/auth.middleware");
 
 router.post(
   "/register",
@@ -23,5 +24,10 @@ router.post("/login", [
 ], 
   userController.loginUser
 )
+
+router.get("/profile", authenticateUser, userController.getUserProfile)
+
+router.get("/logout", authenticateUser, userController.logoutUser)
+
 
 module.exports = router;
